6d6ddcae7929079d56808f76a6a6b9eae7147e82
Juan <josemargri3@gmail.com>
2024-10-17 23:34:54 -0500

    feat: Corregir config diferenciando producción y desarrollo

--- api/config/config.py
+++ api/config/config.py
@@ -1,11 +1,9 @@
 import os
-
-environment = os.getenv('ENVIRONMENT', 'production')
+from dotenv import load_dotenv
+environment = os.getenv('ENVIRONMENT', 'development')
 print(f"Environment: {environment}")
 
 if environment == 'development':
-    from dotenv import load_dotenv
-    # Cargar las variables del archivo .env
     load_dotenv()
     def get_database_url() -> str:
         # Obtiene la URL de la base de datos
@@ -18,12 +16,18 @@ if environment == 'development':
             if not os.path.exists(ruta):
                 ruta = os.path.join('config', 'test.db')
             
-            database_url = f"sqlite:///{ruta}"  # No necesitas reemplazar '\\' por '/', usa os.path.join para mantener la consistencia de rutas
+            database_url = f"sqlite:///{ruta}"
             return database_url
         else:
             # Si la base de datos es de producción
             base_dir.replace('mysql://', 'mysql+pymysql://')
             return base_dir
+        
+    def get_secret_key() -> str:
+        secret_key = os.getenv('SECRET_KEY')
+        if not secret_key:
+            raise ValueError("problema con el JWT.")
+        return secret_key
 else:
     def get_database_url() -> str:
         # Obtiene la URL de la base de datos
@@ -33,9 +37,8 @@ else:
         base_dir = base_dir.replace('mysql://', 'mysql+pymysql://')
         return base_dir
     
-    
-def get_secret_key() -> str:
-    secret_key = os.getenv('SECRET_KEY')
-    if not secret_key:
-        raise ValueError("problema con el JWT.")
-    return secret_key
+    def get_secret_key() -> str:
+        secret_key = os.getenv('SECRET_KEY')
+        if not secret_key:
+            raise ValueError("problema con el JWT.")
+        return secret_key
