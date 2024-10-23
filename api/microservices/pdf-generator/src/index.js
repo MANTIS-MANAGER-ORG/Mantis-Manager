ce26e5394ff8fe9e1f2aa67410671157a3f86ba5
Juan <josemargri3@gmail.com>
2024-10-23 08:52:43 -0500

    feat: Creando nuevas rutas de generacion de pdf

--- api/microservices/pdf-generator/src/index.js
+++ api/microservices/pdf-generator/src/index.js
@@ -3,8 +3,13 @@ const puppeteer = require('puppeteer');
 const fs = require('fs');
 const path = require('path');
 const handlebars = require('handlebars');
+const cors = require('cors');  // <--- Importa cors
 
 const app = express();
+
+// Habilitar CORS para todas las solicitudes
+app.use(cors());
+
 app.use(express.json());
 
 // Función para generar el HTML usando Handlebars
@@ -15,8 +20,25 @@ async function generarHtml(tickets) {
     return template({ tickets });
 }
 
+async function generarHtml2(tickets, reportData) {
+    const templatePath = path.join(__dirname, 'views', 'template2.hbs');
+    const templateHtml = fs.readFileSync(templatePath, 'utf-8');
+    const template = handlebars.compile(templateHtml);
+
+    // reportData debe contener las variables adicionales como fechas y logoUrl
+    const data = {
+        tickets, // los tickets vienen en un array con propiedades detalladas
+        logoUrl: reportData.logoUrl,
+        generationDate: reportData.generationDate,
+        startDate: reportData.startDate,
+        endDate: reportData.endDate
+    };
+
+    return template(data);
+}
+
 // Ruta para generar el PDF
-app.post('/generar-pdf', async (req, res) => {
+app.post('/generar-pdf2', async (req, res) => {
     try {
         const tickets = req.body.tickets; // Recibir tickets desde el cuerpo de la solicitud
 
@@ -34,7 +56,13 @@ app.post('/generar-pdf', async (req, res) => {
         await page.setContent(html, { waitUntil: 'domcontentloaded' });
 
         // Generar el PDF
-        const pdfBuffer = await page.pdf({ format: 'A4' });
+        const pdfBuffer = await page.pdf(
+            {
+                format: 'A4',
+                printBackground: true,
+                //path: 'tickets.pdf',
+            }
+        );
 
         // Cerrar el navegador
         await browser.close();
@@ -53,7 +81,62 @@ app.post('/generar-pdf', async (req, res) => {
     }
 });
 
+// Ruta para generar el PDF
+app.post('/generar-pdf', async (req, res) => {
+    try {
+        const { tickets, generationDate, startDate, endDate, logoUrl } = req.body;
+        //console.log(req.body);
+
+        // Generar el HTML con la información recibida
+        const html = await generarHtml2(tickets, {
+            generationDate,
+            startDate,
+            endDate,
+            logoUrl
+        });
+
+
+        // Iniciar Puppeteer
+        const browser = await puppeteer.launch({
+            args: ['--no-sandbox', '--disable-setuid-sandbox'],
+            headless: true
+        });
+        const page = await browser.newPage();
+
+        // Cargar el HTML generado en la página
+        await page.setContent(html, { waitUntil: 'domcontentloaded' });
+
+        // Generar el PDF
+        const pdfBuffer = await page.pdf({
+            format: 'A4',
+            printBackground: true,
+            //path: 'tickets.pdf',
+            margin: {
+                top: '1in',
+                right: '1in',
+                bottom: '1in',
+                left: '1in'
+            },
+        });
+
+        // Cerrar el navegador
+        await browser.close();
+
+        // Configurar las cabeceras para permitir CORS
+        res.status(200).set({
+            'Access-Control-Allow-Origin': '*',  // Permitir todas las fuentes
+            'Access-Control-Allow-Credentials': true,
+            'Content-Type': 'application/pdf',
+        }).end(pdfBuffer);
+
+    } catch (error) {
+        console.error('Error al generar el PDF:', error);
+        res.status(500).send('Error al generar el PDF');
+    }
+});
+
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
     console.log(`Servidor escuchando en el puerto ${PORT}`);
+    console.log('http://localhost:3000');
 });
